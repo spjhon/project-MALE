295767e4d5769b54880829a7d5b4e72f080645c7
Juan Aristizabal <spjhon@gmail.com>
2024-08-16 10:32:45 -0500

    se agrega contenido basico a la pagina de soluciones

---
+++ src/app/about/page.tsx
@@ -0,0 +1,13 @@
+import { Container } from "@/components/Container";
+
+import { FC } from "react";
+
+const AboutPage: FC = function () {
+  return (
+    <Container>
+      <h1>Conocenos para MalePet Care</h1>
+    </Container>
+  );
+};
+
+export default AboutPage;
---
+++ src/app/pricing/page.tsx
@@ -0,0 +1,13 @@
+import { Container } from "@/components/Container";
+
+import { FC } from "react";
+
+const PricingPage: FC = function () {
+  return (
+    <Container>
+      <h1>Precios para MalePet Care</h1>
+    </Container>
+  );
+};
+
+export default PricingPage;
